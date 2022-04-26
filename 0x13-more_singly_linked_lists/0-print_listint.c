7915e22bb3ddbef6a7dea89a56669c7ed0e4713d
“[Chioma <[sylviacndubuisi@gmail.com]>
2022-04-26 18:18:14 +0100

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 
+
 /**
- *print_listint - prints all the elements of a list.
- *@h: head of a list.
- *
- *Return: numbers of nodes.
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: head of linked list
+ * Return: number nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t i = 0;
+
 
 	while (h != NULL)
 	{
+
 		printf("%d\n", h->n);
 		h = h->next;
-		nnodes++;
+		i++;
 	}
-	return (nnodes);
+
+	return (i);
 }

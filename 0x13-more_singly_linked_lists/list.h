a84f6becb019018576223beb46708d6fc4e6e2d9
“[Chioma <[sylviacndubuisi@gmail.com]>
2022-04-26 17:27:08 +0100

    list

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -5,24 +5,24 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ *struct listint_s - singly linked list
+ *@n: integer
+ *@next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+		int n;
+		struct listint_s *next;
 } listint_t;
 
 /**
  * struct listp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
- *
+ * 
  * Description: singly linked list of pointers
  */
 typedef struct listp_s
@@ -48,3 +48,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+
